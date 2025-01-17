497c15ee506986e90d89df0ed46fc0bcc2c744df
godchuungus <96650932+DarkChungus@users.noreply.github.com>
2025-01-17 09:34:36 +0545

    Create Question 5.c

---
+++ Grade 10 Tutorials/Question 5.c
@@ -0,0 +1,14 @@
+// WAP that displays the following series 10, 9, 8, 7, ..., 1
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	int i=10;
+	while(i>=1){
+		printf("%d, ",i);
+		i--;
+	}
+	// getch();
+	return 0;
+}
